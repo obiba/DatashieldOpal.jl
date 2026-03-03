c6f7dbb3fc1cdc7a138885ad4ee2b041643c12da
Hugo Solleder <hsolleder@users.noreply.github.com>
2026-03-03 12:19:11 +0100

    feat: add datashield internal functions

---
+++ src/datashield_internals.jl
@@ -0,0 +1,18 @@
+"""
+    _datashield_profiles(opal::OpalObject)
+
+Get available Datashield profiles
+
+# Arguments
+- `opal`: An `OpalObject` instance.
+"""
+function _datashield_profiles(opal::OpalObject)
+    if parse(VersionNumber, opal.version) < v"4.2"
+        return Dict{String,Any}("available" => "default", "current" => "default")
+    else
+        return Dict{String,Any}(
+            "available" => map(x -> x["name"], opal_get(opal, "datashield", "profiles")),
+            "current" => isnothing(opal.profile) ? "default" : opal.profile,
+        )
+    end
+end
